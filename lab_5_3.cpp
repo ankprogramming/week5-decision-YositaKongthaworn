38aa2b316712fcbe96bf937b97e071889bffbf4a
Unknown <sodomyoindy@gmail.com>
2017-12-12 12:07:59 +0700

    เสร็จแล้วว

--- lab_5_3.cpp
+++ lab_5_3.cpp
@@ -8,10 +8,22 @@ int main() {
     cout << "Enter your score(0-100) : ";
     cin >> score;
     cout << endl;
-    if(score > 80) {
+    if(score > 100) {
+        cout << "Put wrong score" << endl;
+    } else if (score > 80) {
         cout << "Grade 4" << endl;
+    } else if (score > 75) {
+        cout << "Grade 3.5" << endl;
     } else if (score > 70) {
         cout << "Grade 3" << endl;
+    } else if (score > 65) {
+        cout << "Grade 2.5" << endl;
+    } else if (score > 60) {
+        cout << "Grade 2" << endl;
+    } else if (score > 55) {
+        cout << "Grade 1.5" << endl;
+    } else if (score > 50) {
+        cout << "Grade 1" << endl;
     }
     return 0;
 }
